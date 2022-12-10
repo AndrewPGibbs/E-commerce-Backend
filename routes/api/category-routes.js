ecc01aa394cb8ee8fe07aa760d0e29b424cf258f
Andrew PG <andrewpgibbs71@gmail.com>
2022-12-10 14:28:17 -0500

    added update by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,6 +26,16 @@ try {
   res.status(500).json(err);
 }
 });
+
+//create a category
+router.post('/', async (req, res) => {
+  try {
+    const categoryRes = await Category.create(req.body);
+    res.status(200).json(categoryRes);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 //update a category
 router.post('/', async (req, res) => {
   try {
@@ -39,9 +49,16 @@ router.post('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+ 
+//update category by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryRes = await Category.update( req.body, { where: { id: req.params.id}});
+    res.status(200).json('Category updated');
+  
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
