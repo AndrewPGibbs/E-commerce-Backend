82f09349d47e138abc47c0c4d4269d30119e4efc
Andrew PG <andrewpgibbs71@gmail.com>
2022-12-10 16:50:15 -0500

    routing for delete function in the tag-routes.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,8 +44,17 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+ try {
+  const tagRes = await Tag.destroy({
+    where: {
+      id:req.params.id
+    }
+  });
+  res.status(200).json('Tag OBLITERATED');
+ } catch (err) {
+  res.status(500).json(err);
+ }
 });
 
 module.exports = router;
